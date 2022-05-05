bd509bdcc391b7f8ad3489dccb9a578b190c34cf
Alexey Brovko <brovko-a@live.com>
2022-05-05 21:10:20 +0400

    Return benchmark program

---
+++ src/DotNetEveryDay.Benchmark/Program.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Running;
+
+namespace DotNetEveryDay.Benchmark;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+    }
+
+    [SimpleJob(RunStrategy.Throughput)]
+    public class BenchmarkMethods
+    {
+        [Benchmark]
+        public void FSharp()
+        {
+            
+        }
+
+        [Benchmark]
+        public void CSharp()
+        {
+        }
+    }
+}
